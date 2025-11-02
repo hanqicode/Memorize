c5abe692ecd0d186a9c2cefdb5545061e6a5921f
hanqicode <hanqiumd@gmail.com>
2025-11-01 22:48:47 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Qi Han on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
