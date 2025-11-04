bab7185de43ecd96448001b0619f32bfa8fa7ed6
hanqicode <hanqiumd@gmail.com>
2025-11-03 20:11:54 -0800

    Complete programming assignment 1.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,75 +9,107 @@ import SwiftUI
 
 struct ContentView: View {
 
-    private let emoji: [String] = [
-        "👻", "🎃", "🕷️", "😈", "🙈", "🎶", "🤡", "👹", "💄", "🐝", "🏀", "🏆",
-    ]
-    @State private var cardCount: Int
+    // Constants.
+    private let title: String = "Memorize!"
+    private let vehicles: [String] = ["✈️", "🛳️", "🚀", "🚁"]
+    private let vegetables: [String] = ["🥕", "🥦", "🥬", "🥒", "🌽"]
+    private let sports: [String] = ["⚽", "🏀", "🏐", "🏈", "⚾", "🎾"]
+
+    // Fields.
+    @State private var emojis: [String]
 
     init() {
-        cardCount = 4
+        emojis = vehicles + vehicles
+        emojis = emojis.shuffled()
     }
 
+    // Override.
     var body: some View {
         VStack {
+            gameTitle
+
             ScrollView {
                 cards
             }
 
             Spacer()
 
-            cardAdjusters
+            themeToggler
         }
         .padding()
     }
 
+    var gameTitle: some View {
+        Text(title)
+            .font(.largeTitle)
+            .foregroundStyle(Color.primary)
+    }
+
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120, maximum: 180))]) {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 60, maximum: 180))]) {
             // Like Java lambda expression
             // symbol -> {xxxx};
-            ForEach(0..<cardCount, id: \.self) { index in
-                CardView(content: emoji[index])
+            ForEach(0..<emojis.count, id: \.self) { index in
+                CardView(content: emojis[index])
                     // fit: 完整显示，不裁切
                     .aspectRatio(2 / 3, contentMode: ContentMode.fit)
             }
         }
     }
 
-    var cardAdjusters: some View {
-        HStack {
-            cardRemover
+    var themeToggler: some View {
+        HStack(alignment: .bottom) {
+            Spacer()
+
+            toggleTheme(theme: "Vehicles")
 
             Spacer()
 
-            cardAdder
+            toggleTheme(theme: "Vegetables")
+
+            Spacer()
+
+            toggleTheme(theme: "Sports")
+
+            Spacer()
         }
-        .font(Font.largeTitle)
     }
 
-    var cardRemover: some View {
-        Button(
-            action: {
-                if cardCount > 1 {
-                    cardCount -= 1
-                }
-            },
-            label: {
-                Image(systemName: "minus.rectangle")
-            }
-        )
-    }
+    func toggleTheme(theme: String) -> some View {
+        VStack {
+            Button(
+                action: {
+                    if theme == "Vehicles" {
+                        emojis = vehicles + vehicles
+                    } else if theme == "Vegetables" {
+                        emojis = vegetables + vegetables
+                    } else {
+                        emojis = sports + sports
+                    }
 
-    var cardAdder: some View {
-        Button(
-            action: {
-                if cardCount < emoji.count {
-                    cardCount += 1
+                    emojis = emojis.shuffled()
+
+                    print("Change theme to \(theme)")
+                },
+                label: {
+                    VStack {
+                        if theme == "Vehicles" {
+                            Image(systemName: "car")
+                                .font(.largeTitle)
+                        } else if theme == "Vegetables" {
+                            Image(systemName: "carrot")
+                                .font(.largeTitle)
+                        } else {
+                            Image(systemName: "basketball")
+                                .font(.largeTitle)
+                        }
+                        Text(theme)
+                            .foregroundStyle(Color.blue)
+                            .font(Font.caption)
+                    }
                 }
-            },
-            label: {
-                Image(systemName: "plus.rectangle")
-            }
-        )
+            )
+        }
     }
 }
 
@@ -87,7 +119,7 @@ struct CardView: View {
     private let baseRoundedRectangle: RoundedRectangle
     private let content: String
 
-    init(isFaceUp: Bool = true, content: String) {
+    init(isFaceUp: Bool = false, content: String) {
         self.isFaceUp = isFaceUp
         self.baseRoundedRectangle = RoundedRectangle(cornerRadius: 12)
         self.content = content
@@ -100,7 +132,7 @@ struct CardView: View {
                     .foregroundStyle(Color.white)
                 baseRoundedRectangle
                     .strokeBorder(style: StrokeStyle(lineWidth: 2))
-                    .foregroundStyle(Color.orange)
+                    .foregroundStyle(Color.red)
                 Text(content)
                     .font(Font.largeTitle)
             }
@@ -108,7 +140,7 @@ struct CardView: View {
             .opacity(isFaceUp ? 1 : 0)
 
             baseRoundedRectangle
-                .foregroundStyle(Color.orange)
+                .foregroundStyle(Color.red)
                 .opacity(isFaceUp ? 0 : 1)
         }.onTapGesture {
             isFaceUp.toggle()
